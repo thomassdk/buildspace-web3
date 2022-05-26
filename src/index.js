de71e642456b3eabd6de89b84f4eb48c3cb387c4
thomassdk <thomassdk@pm.me>
2022-05-26 13:18:03 +0200

    Delete unused font

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './astype-bonnet-grotesque-nr-demo.otf';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
